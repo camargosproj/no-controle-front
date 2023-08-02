856c68bc607a5f3c866fff7b93171c90a08f2833
Marcos Camargo <marcoscamargo@outlook.com>
2023-08-02 18:27:41 -0300

    feat: add create and update auth methods

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { IController } from "../core/interfaces";
-import { toAsyncRouter } from "../middlewares/error.middleware";
-import AuthService from "./auth.service";
 import { BadRequestError } from "../errors/custom-errors";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { toAsyncRouter } from "../middlewares/error.middleware";
+import AuthService from "./auth.service";
 
 export default class AuthController implements IController {
   path: string = "/auth";
@@ -17,6 +17,7 @@ export default class AuthController implements IController {
   initializeRoutes(): void {
     this.router.post(this.path + "/singin", this.singIn);
     this.router.post(this.path + "/singup", this.singUp);
+    this.router.patch(this.path, authMiddleware, this.update);
   }
   singIn = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
@@ -31,6 +32,20 @@ export default class AuthController implements IController {
   };
 
   singUp = async (req: Request, res: Response, next: NextFunction) => {
-    throw new Error("Method not implemented.");
+    const { name, email, password } = req.body;
+    if (!email || !password || !name) {
+      throw new BadRequestError("Name, Email and password are required");
+    }
+    const user = await this.authService.singUp(name, email, password);
+    res.status(200).json(user);
+  };
+
+  update = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.authUser;
+    const { name, email, password } = req.body;
+
+    const user = await this.authService.updateUser(id, name, email, password);
+
+    res.status(200).json(user);
   };
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { PrismaService } from "../core/shared";
 import * as jwt from "jsonwebtoken";
+import { PrismaService } from "../core/shared";
 import { UnauthorizedError } from "../errors/custom-errors";
-import { comparePassword } from "../utils/util";
+import { comparePassword, hashPassword } from "../utils/util";
 
 export default class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -37,5 +37,52 @@ export default class AuthService {
     );
     delete userExists.password;
     return { ...userExists, accessToken };
+  }
+
+  async singUp(name: string, email: string, password: string) {
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userExists) {
+      throw new UnauthorizedError("User already exists");
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: await hashPassword(password),
+      },
+    });
+
+    delete newUser.password;
+    return newUser;
+  }
+
+  async updateUser(id: string, name: string, email: string, password: string) {
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!userExists) {
+      throw new UnauthorizedError("User not found");
+    }
+
+    const newUser = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+      select: {
+        name: true,
+      },
+    });
+
+    return newUser;
   }
 }
