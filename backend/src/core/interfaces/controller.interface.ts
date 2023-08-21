d3b452a604ae610aa27363ed66c342bfa7a60a72
Marcos Camargo <marcoscamargo@outlook.com>
2023-08-21 14:48:12 -0300

    refactor: remove backend folder

--- backend/src/core/interfaces/controller.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-export interface IController {
-  path: string;
-  router: Router;
-  isPrivate: boolean;
-  initializeRoutes(): void;
-}
--- backend/src/core/interfaces/service.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IService {
-  create(data: any): Promise<any>;
-  findAll(): Promise<any[]>;
-  findOne(id: string): Promise<any | null>;
-  update(id: string, data: any): Promise<any>;
-  delete(id: string): void;
-}
