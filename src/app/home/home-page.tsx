0048f68d844da1f49950abea3f9eefddad0d5a11
Marcos Camargo <marcoscamargo@outlook.com>
2023-08-31 16:44:05 -0300

    feature: add bar chart and add break point for responsive widgets

--- src/app/home/home-page.tsx
+++ src/app/home/home-page.tsx
@@ -1,13 +1,14 @@
 'use client'
+import SimpleCharts from "../../components/chart/BarChart";
 import Widget from "../../components/widget/Widget";
 import { SummaryResponse } from "./home.type";
 
 
 const HomePage = ({ month, year }: SummaryResponse) => {
     return (
-        <div className={`flex pr-3 pl-3`}>
+        <div className={`flex pr-3 pl-3 w-full`}>
             <div className={`flex flex-col w-full`}>
-                <div className={`flex gap-6 p-2 w-full`}>
+                <div className={`flex gap-6 p-2 w-full flex-col sm:flex-row`}>
                     <Widget type="income" data={{ description: 'Rendimentos', amount: month.totalIncome }} />
                     <Widget type="expense" data={{ description: 'Despesas', amount: month.totalExpense }} />
                     <Widget type="balance" data={{ description: 'Saldo', amount: month.totalBalance }} />
@@ -17,7 +18,7 @@ const HomePage = ({ month, year }: SummaryResponse) => {
                     {/* <Featured /> */}
                     {/* <Chart title="Receita Anual" aspect={2 / 1} />
                     <Chart title="Despesas Anual" aspect={2 / 1} /> */}
-                    {/* <SimpleCharts year={year} /> */}
+                    <SimpleCharts year={year} />
                     {/* <SimpleCharts year={year} /> */}
                 </div>
             </div>
--- src/components/chart/BarChart.tsx
+++ src/components/chart/BarChart.tsx
@@ -1,55 +1,44 @@
 import { BarChart } from "@mui/x-charts";
 import { SummaryResponse } from "../../app/home/home.type";
 
-const data = [
-  { name: "Janeiro", Total: 1200 },
-  { name: "Fevereiro", Total: 2100 },
-  { name: "Março", Total: 800 },
-  { name: "Abril", Total: 1600 },
-  { name: "Maio", Total: 900 },
-  { name: "Junho", Total: 1700 },
-  { name: "Julho", Total: 1300 },
-  { name: "Agosto", Total: 1300 },
-  { name: "Setembro", Total: 1500 },
-  { name: "Outubro", Total: 2000 },
-  { name: "Novembro", Total: 2700 },
-  { name: "Dezembro", Total: 1100 },
-];
+
 
 export default function SimpleCharts({ year }: Partial<SummaryResponse>) {
   return (
-    <BarChart
-      xAxis={[
-        {
-          id: "barCategories",
-          data: year.map((d) => d.month),
-          scaleType: "band",
-        },
-      ]}
-      series={[
-        {
-          data: year.map((d) => d.expenseTotal),
-          label: "Despesas",
-          color: "red",
-        },
-        {
-          data: year.map((d) => d.incomeTotal),
-          label: "Receitas",
-          color: "green",
+    <>
+      {year?.length ? <BarChart
+        xAxis={[
+          {
+            id: "barCategories",
+            data: year.map(({ month }) => month),
+            scaleType: "band",
+          },
+        ]}
+        series={[
+          {
+            data: year.map(({ expenseTotal }) => expenseTotal),
+            label: "Despesas",
+            color: "red",
+          },
+          {
+            data: year.map(({ incomeTotal }) => incomeTotal),
+            label: "Receitas",
+            color: "green",
 
-        },
-        {
-          data: year.map((d) => d.balance),
-          label: "Saldo",
-          color: "#15637b",
+          },
+          {
+            data: year.map(({ balance }) => balance),
+            label: "Saldo",
+            color: "#15637b",
 
-        },
-      ]}
-      className={`text-primary bg-primary`}
-      colors={["#15637b"]}
+          },
+        ]}
+        className={`text-primary bg-primary`}
+        colors={["#15637b"]}
 
-      width={800}
-      height={500}
-    />
+        width={800}
+        height={500}
+      /> : null}
+    </>
   );
 }
