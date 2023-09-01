695b729dfea491064dad10dafd2b6c2d6ecd9922
Marcos Camargo <marcoscamargo@outlook.com>
2023-09-01 08:09:05 -0300

    feature: use month in portuguese for the chart

--- src/components/chart/BarChart.tsx
+++ src/components/chart/BarChart.tsx
@@ -1,16 +1,22 @@
 import { BarChart } from "@mui/x-charts";
 import { SummaryResponse } from "../../app/home/home.type";
+import { MonthMap } from "../filter/Filter";
 
 
 
 export default function SimpleCharts({ year }: Partial<SummaryResponse>) {
+  const reversedMonthMap = {};
+  for (const key in MonthMap) {
+    reversedMonthMap[MonthMap[key]] = key;
+  }
+
   return (
     <>
       {year?.length ? <BarChart
         xAxis={[
           {
             id: "barCategories",
-            data: year.map(({ month }) => month),
+            data: year.map(({ month }) => reversedMonthMap[month.toLowerCase()].toUpperCase()),
             scaleType: "band",
           },
         ]}
