c7f689925d03c8b95ac5c55841cb5fbe52a86612
Marcos Camargo <marcoscamargo@outlook.com>
2023-09-01 17:15:47 -0300

    feature: make chart a bit responsive and add 12 months to fill the chart

--- src/app/home/home-page.tsx
+++ src/app/home/home-page.tsx
@@ -6,7 +6,7 @@ import { SummaryResponse } from "./home.type";
 
 const HomePage = ({ month, year }: SummaryResponse) => {
     return (
-        <div className={`flex pr-3 pl-3 w-full`}>
+        <div className={`flex pr-3 pl-3 w-full h-full`}>
             <div className={`flex flex-col w-full`}>
                 <div className={`flex gap-6 p-2 w-full flex-col sm:flex-row`}>
                     <Widget type="income" data={{ description: 'Rendimentos', amount: month.totalIncome }} />
@@ -14,12 +14,11 @@ const HomePage = ({ month, year }: SummaryResponse) => {
                     <Widget type="balance" data={{ description: 'Saldo', amount: month.totalBalance }} />
                 </div>
 
-                <div className={`flex flex-col md:flex-row`}>
-                    {/* <Featured /> */}
-                    {/* <Chart title="Receita Anual" aspect={2 / 1} />
-                    <Chart title="Despesas Anual" aspect={2 / 1} /> */}
+
+                <div className={`flex flex-col items-center justify-center md:flex-row w-full h-full `}>
+
                     <SimpleCharts year={year} />
-                    {/* <SimpleCharts year={year} /> */}
+
                 </div>
             </div>
         </div >
--- src/components/chart/BarChart.tsx
+++ src/components/chart/BarChart.tsx
@@ -2,49 +2,60 @@ import { BarChart } from "@mui/x-charts";
 import { SummaryResponse } from "../../app/home/home.type";
 import { MonthMap } from "../filter/Filter";
 
-
-
 export default function SimpleCharts({ year }: Partial<SummaryResponse>) {
   const reversedMonthMap = {};
   for (const key in MonthMap) {
     reversedMonthMap[MonthMap[key]] = key;
   }
 
+  // Create an array of 12 months to add data from the year to the graph
+  const months = Object.values(MonthMap).map((month) => {
+    const monthData = year.find((item) => item.month === month.toUpperCase());
+    return {
+      month: reversedMonthMap[month].toUpperCase().slice(0, 3),
+      expenseTotal: monthData?.expenseTotal || 0,
+      incomeTotal: monthData?.incomeTotal || 0,
+      balance: monthData?.balance || 0,
+    };
+  });
+
   return (
     <>
-      {year?.length ? <BarChart
-        xAxis={[
-          {
-            id: "barCategories",
-            data: year.map(({ month }) => reversedMonthMap[month.toLowerCase()].toUpperCase()),
-            scaleType: "band",
-          },
-        ]}
-        series={[
-          {
-            data: year.map(({ expenseTotal }) => expenseTotal),
-            label: "Despesas",
-            color: "red",
-          },
-          {
-            data: year.map(({ incomeTotal }) => incomeTotal),
-            label: "Receitas",
-            color: "green",
+      {months?.length ? (
 
-          },
-          {
-            data: year.map(({ balance }) => balance),
-            label: "Saldo",
-            color: "#15637b",
 
-          },
-        ]}
-        className={`text-primary bg-primary`}
-        colors={["#15637b"]}
+        <div className={`w-full h-[80%]`}>
 
-        width={800}
-        height={500}
-      /> : null}
+          <BarChart
+            xAxis={[
+              {
+                id: "barCategories",
+                data: months.map(({ month }) => month),
+                scaleType: "band",
+              },
+            ]}
+            series={[
+              {
+                data: months.map(({ expenseTotal }) => expenseTotal),
+                label: "Despesas",
+                color: "red",
+              },
+              {
+                data: months.map(({ incomeTotal }) => incomeTotal),
+                label: "Receitas",
+                color: "green",
+              },
+              {
+                data: months.map(({ balance }) => balance),
+                label: "Saldo",
+                color: "#15637b",
+              },
+            ]}
+            className={`text-primary bg-primary`}
+            colors={["#15637b"]}
+          />
+        </div>
+      ) : null}
     </>
   );
 }
