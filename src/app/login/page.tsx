36f92dc8903c286bcf5624224046611d258032da
Marcos Camargo <marcoscamargo@outlook.com>
2023-09-05 14:32:59 -0300

    feature: add form validation using joi

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,50 @@
 "use client";
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
 import Link from "next/link";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { MdPaid } from "react-icons/md";
+import { toast } from "react-toastify";
 import { useAuth } from "../../services/contexts/useAuth";
+
+const schema = Joi.object({
+    email: Joi.string()
+        .required()
+        .email({
+            tlds: false,
+        })
+        .message("Email inválido"),
+    password: Joi.string()
+        .required()
+        .min(6)
+        .message("Senha deve ter no mínimo 6 caracteres"),
+});
 
 const Login = () => {
     const auth = useAuth();
 
-    const { register, handleSubmit, formState } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm({
+        resolver: joiResolver(schema),
+    });
 
     const handleLogin = (data: { email: string; password: string }) => {
         auth.login(data);
     };
+
+    useEffect(() => {
+        Object.keys(errors).forEach((key) => {
+            toast(errors[key].message as string, {
+                type: "error",
+                autoClose: 3000,
+                closeButton: true,
+            });
+        });
+    }, [errors]);
 
     return (
         <div className="flex flex-col w-full h-screen items-center justify-center gap-5 ">
@@ -41,15 +74,13 @@ const Login = () => {
                     placeholder="senha"
                 />
                 <div className="flex w-full gap-3 justify-between">
-
-                    <Link href={`register`} >Criar conta</Link>
+                    <Link href={`register`}>Criar conta</Link>
                     <Link href={`#`}>Esqueceu a senha?</Link>
                 </div>
                 <button
                     type="submit"
                     className="bg-primary w-full hover:bg-secondary text-white hover:text-primary px-4 py-2 rounded-md"
-                    disabled={formState.isSubmitting}
-
+                    disabled={isSubmitting}
                 >
                     Entrar
                 </button>
